9a7c399194b69558ba8b3a5778eb6b46b2a43a23
jiaochenli <jiaochenli@gmail.com>
2019-03-07 11:32:01 +0800

    添加实体类和拦截器

--- src/main/java/com/itsun/web/controller/IndexController.java
+++ src/main/java/com/itsun/web/controller/IndexController.java
@@ -1,6 +1,6 @@
 package com.itsun.web.controller;
 
-import com.itsun.domain.User;
+import com.itsun.domain.UserVo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,7 +10,7 @@ public class IndexController {
 
 
     @GetMapping("/login")
-    public String index(User user){
+    public String index(UserVo user){
         return "admin-index";
     }
 
